8f433f604551408f8b714c76a7bad48ae37dd50e
the_new_sky <lorisazerty@gmail.com>
2016-10-23 22:03:58 +0200

    CopyMan V2
    Changelog :
    
    - Cleaner code
    - Use Click and pyftplib
    - Zip support
    - FTP server support
    - New interface

--- copyman.py
+++ copyman.py
@@ -1,89 +1,106 @@
-import shutil
-import sys
-import os
-import datetime
-import tarfile
+import click  # Command line interface framework
+import shutil  # File system utilities
+import os.path
+import errno  # Exception identification
+import tarfile  # Archive and compression formats
+import zipfile
 
-def copy_file(src, dest):
-    """Simple function to copy files or directory"""
-    dest_file = os.path.split(src)
-    try:
-        dest_eval = os.path.join(dest, dest_file[1])
-        try:
-            shutil.copyfile(src, dest_eval)
-            return 'ok'
-        #src and dest are the same file
-        except shutil.Error:
-            print 'Error : Source and Destination are the same file.'
-            return 'oups'
-        #src or dest doesn't exist
-        except IOError as e:
-            print 'Log : ' + str(e)
+# FTP server library
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+
+@click.group()
+def cli():
+    """
+    Welcome to CopyMan V2 !
+
+    The CLI tool for copy, compress and shares files and folders easily !
+    """
+    pass
+
+
+@cli.command('gather')
+@click.option('--destination', '-d', type=click.Path(exists=True, dir_okay=True), prompt="Destination folder path",
+              help="The folder where all file will be copied.")
+@click.option('--sources', '-s', type=click.Path(exists=True), multiple=True,
+              help="All the files/folders to copy.")
+@click.option('--ignore', '-i', default="", multiple=True, help="Filenames and patterns to ignore in folders copy.")
+def gather(destination, sources, ignore):
+    """Gather files in a same destination."""
+
+    destination = click.format_filename(destination)  # Convert Path to string
+
+    with click.progressbar(sources) as copy_bar:
+        for data in copy_bar:
+            data = click.format_filename(data)  # Here too
+
             try:
-                shutil.copytree(src, dest_eval)
-                return 'ok'
-                #src and dest are the same file
-            except shutil.Error:
-                print 'Error : Source and Destination are the same file.'
-                return 'oups'
-	        #src or dest doesn't exist
-            except IOError as e:
-                print 'Log : ' + str(e)
-                return 'oups'
-    except IndexError:
-        dest_eval = dest
-        try:
-            shutil.copytree(src, dest_eval)
-            return 'ok'
-        #src and dest are the same file
-        except shutil.Error:
-            print 'Error : Source and Destination are the same file.'
-            return 'oups'
-	    #src or dest doesn't exist
-        except IOError as e:
-            print 'Log : ' + str(e)
-            return 'oups'
-		
+                shutil.copytree(data, "{}/{}".format(destination, data), ignore=shutil.ignore_patterns(*ignore))
+                click.echo(" Folder '{}' successfully copied in destination folder.".format(data))
 
-def copy_lot_file(srclst, to):
-    """An extension to copy_file() to allow you to copy lot of files."""
-    print 'Copying files, please wait...'
-    for copy in srclst:
-        log = copy_file(copy, to)
-        if log == 'ok':
-            print 'Copying successful'
-		
-def generate_namefile():
-    """A simple function who return the name of the tar.gz file."""
-    today = datetime.date.today()
-    return str(today) + "-Package.tar.gz"
-	
-def targz_file(srclst, name):
-    """Function who compress in a tar.gz file some other files."""
-    print 'Compressing files, please wait...'
-    targz =  tarfile.open(name, "w:gz")
-    for file in srclst:
-        targz.add(file)
-    targz.close()
-	
-def help(error=False):
-    if error:
-        print "Unrecognized command, see help here."
+            except OSError as dir_error:
+                if dir_error.errno == errno.ENOTDIR:  # If data is a file, not a directory
+                    shutil.copy(data, destination)
+                    click.echo(" File '{}' successfully copied in destination folder.".format(data))
+                else:
+                    click.echo(" Error : Python {}".format(dir_error))
 
-    print " Commands :"
-    print ""
-    print "python copyman.py help : to get help"
-    print ""
-    print "python copyman.py copy <destination> <source1> <source2> <source3> ... : to copy a list of files/directories in an unique directory"
-    print ""
-    print "python copyman.py tar <destination> <source1> <source2> <source3> ... :  to compress (tar.gz) a list of files/directories in an unique file."
 
-command = sys.argv[1]
-if command == "help":
-    help()
-elif command == "copy":
-    copy_lot_file(sys.argv[3:], sys.argv[2])
-elif command == "tar":
-    targz_file(sys.argv[3:], sys.argv[2] +"/"+ generate_namefile())
-else:
-    help(error=True)
+@cli.command('targz')
+@click.option('--name', '-n', type=click.Path(), default="Package.tar.gz",
+              help="The file where all file will be packaged and compressed.")
+@click.option('--sources', '-s', type=click.Path(exists=True), multiple=True,
+              help="All the files/folders to package.")
+def archive_targz(name, sources):
+    """Gather files in a .tar.gz package."""
+
+    name = click.format_filename(name)
+
+    with tarfile.open(name, "w:gz") as tar_file:
+        with click.progressbar(sources) as tar_bar:
+            for data in tar_bar:
+                data = click.format_filename(data)
+
+                tar_file.add(data, arcname=os.path.basename(data))
+                click.echo(" '{}' successfully packaged and compressed.".format(os.path.basename(data)))
+
+
+@cli.command('zip')
+@click.option('--name', '-n', type=click.Path(), default="Package.zip",
+              help="The file where all file will be zipped.")
+@click.option('--sources', '-s', type=click.Path(exists=True), multiple=True,
+              help="All the files/folders to zip.")
+def archive_zip(name, sources):
+    """Gather files in a zip package."""
+
+    name = click.format_filename(name)
+
+    with zipfile.ZipFile(name, "w", zipfile.ZIP_DEFLATED) as zip_file:
+        with click.progressbar(sources) as zip_bar:
+            for data in zip_bar:
+                data = click.format_filename(data)
+
+                zip_file.write(data, arcname=os.path.basename(data))
+                click.echo(" '{}' successfully zipped.".format(os.path.basename(data)))
+
+
+@cli.command('serve')
+@click.option('--ip', default="127.0.0.1", help="IP address to serve.")
+@click.option('--port', default=2221, help="Port to serve.")
+@click.option('--directory', type=click.Path(exists=True, dir_okay=True), help="Directory to serve.")
+def serve(ip, port, directory):
+    """Serve files on a FTP server."""
+
+    authorizer = DummyAuthorizer()
+    authorizer.add_anonymous(click.format_filename(directory))
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    server = FTPServer((ip, port), handler)
+
+    click.echo("FTP Server successfully configurated at {}:{}.".format(ip, port))
+    server.serve_forever()
+
+if __name__ == '__main__':
+    cli()
